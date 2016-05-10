6b3dce02d402e28a9c94069dea5c1ccdcb9290ec
Justin Grover <dancingcactus@gmail.com>
2016-05-10 15:22:10 -0600

    clean up a bit for public consumption

--- runner.py
+++ runner.py
@@ -1,9 +1,5 @@
 from DataInsertion import DataInsertion
 
-
-request = DataInsertion("http://justingrover.112.2o7.net/b/ss//6","blogdev")
-request.add_variable("pageName","Analytics Launch eVent v1.1")
-request.add_variable("contextData",{"a.LaunchEvent":1})
-print request.variables
+request = DataInsertion("http://justingrover.112.2o7.net/b/ss//6","somereportsuite")
+request.add_variable("pageName","somepagename")
 request.send_request()
-print request.request_data
